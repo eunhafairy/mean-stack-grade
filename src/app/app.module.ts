dc896e3a811f539ab17c856f372a55e1f67649e0
iver <ipesiganifc@gmail.com>
2022-03-25 14:09:33 +0800

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './elements/header/header.component';
+import { ButtonComponent } from './elements/button/button.component';
+import { InputTextComponent } from './elements/input-text/input-text.component';
+import { SignInComponent } from './page/sign-in/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    InputTextComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/elements/input-text/input-text.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-text',
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.css']
+})
+export class InputTextComponent implements OnInit {
+
+@Input() placeholder?: string;
+@Input() type?: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
